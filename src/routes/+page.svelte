f29bf77837620ec24a3e68998387847b6d0718f2
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2023-06-10 14:53:29 +0900

    Install Editor.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let div: HTMLDivElement;
+
+	onMount(async () => {
+		const EditorJS = await import('@editorjs/editorjs');
+		const editor = new EditorJS.default({
+			holder: div
+		});
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div bind:this={div} />
